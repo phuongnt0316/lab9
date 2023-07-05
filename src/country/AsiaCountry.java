32cc4bc7a4620eeda2be9bbe10d2d4f75ae173b6
Phuong Nguyen <phuong16397@gmail.com>
2023-07-05 16:59:55 +0700

    Country

--- src/country/AsiaCountry.java
+++ src/country/AsiaCountry.java
@@ -3,11 +3,8 @@ package country;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import motor.yamaha.Jupiter;
-import motor.yamaha.Serius;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -19,22 +16,26 @@ public class AsiaCountry extends Country {
 
 
     public static void main(String[] args) {
+        boolean cont;
         do {
             List<AsiaCountry> lista = new ArrayList<AsiaCountry>();
             int n;
             System.out.println(" Chon chuc nang 1-5:");
             Scanner sc = new Scanner(System.in);
             n = Integer.parseInt(sc.nextLine());
-            boolean cont = true;
+            cont = true;
             switch (n) {
                 case 1:
                     System.out.println();
+                    cont = false;
+                    break;
+
 
                 default:
                     System.out.println("Nhap lai: ");
                     break;
             }
-        }
+        } while (cont);
     }
 }
 
